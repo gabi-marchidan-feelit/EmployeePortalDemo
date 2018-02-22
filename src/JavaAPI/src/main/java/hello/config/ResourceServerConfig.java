c64dc820f286142daff9597be4b18bc748d99f61
Gabriel Marchidan <gabi.marchidan@live.com>
2018-02-22 09:59:13 -0500

    Added JavaSpring Api

---
+++ src/JavaAPI/src/main/java/hello/config/ResourceServerConfig.java
@@ -0,0 +1,35 @@
+package hello.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/info**").permitAll()
+                .antMatchers("/secret**").authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        TokenStore tokenStore = new InMemoryTokenStore();
+        resources.resourceId("api1")
+                .tokenStore(tokenStore);
+    }
+}
---
+++ src/JavaAPI/src/main/java/hello/controllers/InfoController.java
@@ -0,0 +1,13 @@
+package hello.controllers;
+
+import hello.models.InfoGetResponseModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    @RequestMapping("/info")
+    public InfoGetResponseModel info() {
+        return new InfoGetResponseModel("Info");
+    }
+}
