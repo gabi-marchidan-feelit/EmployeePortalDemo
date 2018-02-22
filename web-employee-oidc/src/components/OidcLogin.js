0f60e7ed48c2438b3c51e1339f90a958e0de42cc
alinpavel <alin.pavel@feel-it-services.com>
2018-02-22 15:36:25 -0500

    added json view theme

--- web-employee-oidc/src/components/OidcLogin.js
+++ web-employee-oidc/src/components/OidcLogin.js
@@ -81,6 +81,26 @@ export default class OidcLogin extends Component {
     const {currentUser, objectToBeautify} = this.state;
     // const json = JSON.parse(objectToBeautify);
     const json = JSON.parse(objectToBeautify);
+    const theme = {
+      scheme: 'monokai',
+      author: 'wimer hazenberg (http://www.monokai.nl)',
+      base00: '#272822',
+      base01: '#383830',
+      base02: '#49483e',
+      base03: '#75715e',
+      base04: '#a59f85',
+      base05: '#f8f8f2',
+      base06: '#f5f4f1',
+      base07: '#f9f8f5',
+      base08: '#f92672',
+      base09: '#fd971f',
+      base0A: '#f4bf75',
+      base0B: '#a6e22e',
+      base0C: '#a1efe4',
+      base0D: '#66d9ef',
+      base0E: '#ae81ff',
+      base0F: '#cc6633'
+    };
 		return (
 			<div>
         <button onClick={this.login}>Login</button>
@@ -91,7 +111,7 @@ export default class OidcLogin extends Component {
           You are currently logged in with SID: {currentUser}
         </div>
         <div className="json-display">
-          <JSONTree data={json} />
+          <JSONTree data={json} theme={theme} invertTheme={false} />
         </div>
       </div>
 		);
